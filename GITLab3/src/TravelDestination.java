9241eea5e5682d246f6193912742a018698e9e6b
Jack-Huerta11 <100225776+Jack-Huerta11@users.noreply.github.com>
2022-02-22 12:15:31 -0600

    Added Println
    Added println with My name and a place that I would love to travel to one day

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -4,6 +4,7 @@ public class TravelDestination {
 	public static void main(String[] args) {
 				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
 				System.out.println("My name is Hunter Drummond. I wish to travel to Japan one day !");
+				System.out.println("My name is Jack Huerta. I wish to travel to New Zealand one day !");
 	}
 
 }
