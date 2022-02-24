2e24e5cb59d1961b426cbe1c6afcead2305f1a8e
Antonio Maldonado <opusx123@yahoo.com>
2022-02-24 12:10:07 -0600

    added two git status

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -8,7 +8,7 @@ public class TravelDestination {
 				System.out.println("My name is Peter Banh. I wish to travel to Paris one day !");
 				System.out.println("My name is Nikhil Kapoor. I wish to travel to Egypt one day !");
 				System.out.println("My name is Hezron Eric Perez. I may wish to travel to Colorado one day !!!");
-				System.out.println("My name is Antonio Maldonado. I may wish to travel to China one day !");
+				System.out.println("My name is Antonio Maldonado. I may wish to travel to China one day !!!");
 				System.out.println("My name is Derek Torres. I wish to travel to Dubai one day !");
 
 
