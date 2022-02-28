00fe535795e8cf1edd1a9e2afdf1c00517bdcaaf
Cuatesoo <64369162+Cuatesoo@users.noreply.github.com>
2022-02-28 15:15:19 -0600

    Update TravelDestination.java
    added name and travel destination

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -4,6 +4,7 @@ public class TravelDestination {
 	public static void main(String[] args) {
 				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
 				System.out.println("My name is Hunter Drummond. I wish to travel to Japan one day !");
+				System.out.println("My name is Mateo Rivas I wish to travel to Germany one day !");
 	}
 
 }
