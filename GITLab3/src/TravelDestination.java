d9e7d5a63164005cbf7527499490094078fe6b8a
David Lambert <dlambert124@utexas.edu>
2022-03-01 14:46:26 -0600

    first commit to travel.java

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -35,9 +35,8 @@ public class TravelDestination {
 				System.out.println("My name is Brandon Chung. I wish to travel to Japan one day !");				
 				System.out.println("My name is Christian Flores. I wish to travel to Greece one day !");
 				System.out.println("My name is Reem Salem. I wish to travel to Italy one day !");
-
-
-     
+				System.out.println("My name is David Lambert. I wish to travel to Australia one day !");
+    
 	}
 
 }
